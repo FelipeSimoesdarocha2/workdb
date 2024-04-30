44436d81a8782d493cbb2d1eda42dd53a1c982d1
Felipe Simoes da rocha <felipe@waiuru.com>
2024-04-29 22:37:33 -0300

    init

--- src/components/header/Header.styles.ts
+++ src/components/header/Header.styles.ts
@@ -4,17 +4,25 @@ export const Component = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: fixed;
+  width: 100%;
+  height: 100px;
   background-color: #235c88;
   border-right: 1px solid #e9ebf1;
   box-shadow: 0px 8px 10px 3px #0000001a;
   z-index: 100;
-  height: 100px;
+  transition: all 500ms ease-out;
 
- &.active {
+  &.active {
     visibility: hidden;
     opacity: 0;
     height: 0px;
     transition: all 750ms;
+
+    > div {
+      filter: blur(2rem);
+      transition: all 1s;
+    }
   }
 `;
 
